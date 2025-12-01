0033f4857cc8e0d139781472bb9348535fcb226d
Ed Salkeld <edward.salkeld@brightnetwork.co.uk>
2025-12-01 11:37:16 +0000

    biome formatting

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const messagesTable = sqliteTable("messages_table", {
-  id: int().primaryKey({ autoIncrement: true }),
-  message: text().notNull(),
-  timestamp: int({ mode: "timestamp" }).notNull(),
+	id: int().primaryKey({ autoIncrement: true }),
+	message: text().notNull(),
+	timestamp: int({ mode: "timestamp" }).notNull(),
 });
